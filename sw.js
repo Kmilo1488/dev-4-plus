b4eb0393565f9a9ea81e0b0a906a76f8f25af0c5
Kmilo1488 <camilomoreno1488@live.com>
2020-11-19 18:21:21 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-18f801031ffafa93bab3.js"
+    "url": "webpack-runtime-0546e236a01a87cf20ef.js"
   },
   {
     "url": "framework-83e11af5cf111be5ad89.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7cf1efdfa8513a47e3bd1121de73993f"
+    "revision": "9d823733f68316c49ba8404cfd4af0df"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "2e9b8952b014e6fb34089976b7b13c94"
+    "revision": "da6f53bf4b2b5a51d344d21a8f3ec770"
   },
   {
     "url": "polyfill-4b1e8b7e04c413f9811f.js"
